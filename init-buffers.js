ac4bab20feea1ce624a82100d07611690616e0f7
Laszlo Kocsis <kocsislaci95@gmail.com>
2026-02-12 11:41:37 +0100

    feat(webgl): implement basic cone light source and texture loading

--- init-buffers.js
+++ init-buffers.js
@@ -1,21 +1,79 @@
 function initBuffers(gl) {
-    const positionBuffer = initPositionBuffer(gl);
+    const position = initPositionBuffer(gl);
+    const normal = initNormalBuffer(gl);
+    const texCoord = initTexCoordBuffer(gl);
+    const color = initColorBuffer(gl);
   
     return {
-      position: positionBuffer,
+      position,
+      normal,
+      texCoord,
+      color,
     };
   }
   
 function initPositionBuffer(gl) {
     const positionBuffer = gl.createBuffer();
-  
     gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
   
-    const positions = [1.0, 1.0, -1.0, 1.0, 1.0, -1.0, -1.0, -1.0];
+    // 3D positions for quad (z=0)
+    const positions = [
+      1.0, 1.0, 0.0,
+      -1.0, 1.0, 0.0,
+      1.0, -1.0, 0.0,
+      -1.0, -1.0, 0.0,
+    ];
   
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
-  
     return positionBuffer;
+  }
+
+function initNormalBuffer(gl) {
+    const normalBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, normalBuffer);
+  
+    // All normals point toward camera (0, 0, 1)
+    const normals = [
+      0.0, 0.0, 1.0,
+      0.0, 0.0, 1.0,
+      0.0, 0.0, 1.0,
+      0.0, 0.0, 1.0,
+    ];
+  
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(normals), gl.STATIC_DRAW);
+    return normalBuffer;
+  }
+
+function initTexCoordBuffer(gl) {
+    const texCoordBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, texCoordBuffer);
+  
+    // UV coordinates for quad corners
+    const texCoords = [
+      1.0, 1.0,
+      0.0, 1.0,
+      1.0, 0.0,
+      0.0, 0.0,
+    ];
+  
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(texCoords), gl.STATIC_DRAW);
+    return texCoordBuffer;
+  }
+
+function initColorBuffer(gl) {
+    const colorBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+  
+    // RGBW colors for quad corners (Red, Green, Blue, White)
+    const colors = [
+      1.0, 0.0, 0.0, 1.0,  // Red (top-right)
+      0.0, 1.0, 0.0, 1.0,  // Green (top-left)
+      0.0, 0.0, 1.0, 1.0,  // Blue (bottom-right)
+      1.0, 1.0, 1.0, 1.0,  // White (bottom-left)
+    ];
+  
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+    return colorBuffer;
   }
   
 export { initBuffers };
--- webgl.js
+++ webgl.js
@@ -1,6 +1,8 @@
 import { initBuffers } from './init-buffers.js';
 import { drawScene } from './draw-scene.js';
 
+const mouseState = { x: 0.0, y: 0.0 };
+
 main();
 
 async function main() {
@@ -14,6 +16,13 @@ async function main() {
       return;
     }
 
+    // Mouse tracking
+    canvas.addEventListener('mousemove', (event) => {
+        const rect = canvas.getBoundingClientRect();
+        mouseState.x = ((event.clientX - rect.left) / rect.width) * 2 - 1;
+        mouseState.y = -(((event.clientY - rect.top) / rect.height) * 2 - 1);
+    });
+
     const vertexShaderSource = await loadShaderFile('./vertex.glsl');
     const fragmentShaderSource = await loadShaderFile('./fragment.glsl');
     const shaderProgram = initShaderProgram(gl, vertexShaderSource, fragmentShaderSource);
@@ -22,15 +31,59 @@ async function main() {
         program: shaderProgram,
         attribLocations: {
           vertexPosition: gl.getAttribLocation(shaderProgram, "aVertexPosition"),
+          vertexNormal: gl.getAttribLocation(shaderProgram, "aVertexNormal"),
+          texCoord: gl.getAttribLocation(shaderProgram, "aTexCoord"),
+          vertexColor: gl.getAttribLocation(shaderProgram, "aVertexColor"),
         },
         uniformLocations: {
           projectionMatrix: gl.getUniformLocation(shaderProgram, "uProjectionMatrix"),
           modelViewMatrix: gl.getUniformLocation(shaderProgram, "uModelViewMatrix"),
+          texture: gl.getUniformLocation(shaderProgram, "uTexture"),
+          lightPos: gl.getUniformLocation(shaderProgram, "uLightPos"),
+          lightDir: gl.getUniformLocation(shaderProgram, "uLightDir"),
+          coneAngle: gl.getUniformLocation(shaderProgram, "uConeAngle"),
+          coneSoftness: gl.getUniformLocation(shaderProgram, "uConeSoftness"),
+          cameraPos: gl.getUniformLocation(shaderProgram, "uCameraPos"),
         },
     };
     const buffers = initBuffers(gl);
+    const texture = loadTexture(gl);
 
-    drawScene(gl, programInfo, buffers);
+    function render() {
+        drawScene(gl, programInfo, buffers, texture, mouseState);
+        requestAnimationFrame(render);
+    }
+    render();
+}
+
+function loadTexture(gl) {
+    const texture = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+
+    // Placeholder 1x1 pixel texture while image loads
+    const pixel = new Uint8Array([255, 100, 150, 255]);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 1, 1, 0, gl.RGBA, gl.UNSIGNED_BYTE, pixel);
+
+    const image = new Image();
+    image.onload = () => {
+        gl.bindTexture(gl.TEXTURE_2D, texture);
+        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+
+        if (isPowerOf2(image.width) && isPowerOf2(image.height)) {
+            gl.generateMipmap(gl.TEXTURE_2D);
+        } else {
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+        }
+    };
+    image.src = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP8z8DwHwAFBQIAX8jx0gAAAABJRU5ErkJggg==';
+
+    return texture;
+}
+
+function isPowerOf2(value) {
+    return (value & (value - 1)) === 0;
 }
 
 async function loadShaderFile(url) {
